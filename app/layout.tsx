1f58150d54b188dd4f251529741a6a0f44ef81fd
zhangpeng <zhangpeng@wedobest.com.cn>
2024-10-29 09:45:33 +0800

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,16 @@ import { GoogleAnalytics } from "@next/third-parties/google";
 
 export const metadata: Metadata = {
   title: "Transformers WebUI",
-  description: "Power by Transformers.js",
+  description:
+    "Interactive web interface for running Transformers.js models. Perform machine learning tasks directly in your browser.",
+  keywords: "transformers.js, machine learning, AI, web interface, browser ML",
+  icons: {
+    icon: "/logo.svg",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({
